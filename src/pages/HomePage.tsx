bc8145b5db7350021e9f1b91ae214bbe01320108
André Leme <andreleme08@gmail.com>
2026-05-12 23:10:38 -0300

    feat: adicionar testes de qualidade de código #21

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,19 +4,27 @@ import type { Product } from '../types/product.types.ts';
 
 const HomePage = () => {
   const [product, setProduct] = useState<Product | null>(null);
+  const [erro, setErro] = useState(false);
 
   useEffect(() => {
     getFirstProduct()
       .then(data => setProduct(data))
-      .catch(err => console.error(err));
+      .catch(() => setErro(true));
   }, []);
 
-  if (!product) return <div>Carregando prova de conceito...</div>;
+  if (erro) {
+    return <div>Erro ao carregar produto.</div>;
+  }
+
+  if (!product) {
+    return <div>Carregando prova de conceito...</div>;
+  }
 
   return (
     <main>
       <h1>PoC: Conexão com API</h1>
-      <div style={{ border: '1px solid #ccc', padding: '1rem' }}>
+
+      <div className="product-card">
         <h2>{product.title}</h2>
         <p>{product.description}</p>
         <p>Preço: ${product.price}</p>
