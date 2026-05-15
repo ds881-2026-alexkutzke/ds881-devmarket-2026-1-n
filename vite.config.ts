fc8109c3fdd60aa4199563eeca98d757e7dd55b7
Angarius <angelo.prebianca@gmail.com>
2026-05-15 15:56:49 -0300

    Set base path based on CI environment
    troca a url base do projeto quando estiver em CI

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.CI ? '/ds881-devmarket-2026-1-n/' : '/',
   plugins: [
     tailwindcss(),
     react()
