6ef144fc20827eb98ebd1383ac02626de8a0c104
André Leme <andreleme08@gmail.com>
2026-05-15 23:48:46 -0300

    desenvolvido debounce e criada pasta utils/

---
+++ src/utils/debounce.ts
@@ -0,0 +1,24 @@
+ /**
+  * Cria uma versão "debounced" da função recebida.
+  *
+  * Uso típico:
+  * Evitar múltiplas chamadas consecutivas em inputs de busca,
+  * aguardando o usuário parar de digitar antes de executar a função.
+  *
+  * Exemplo:
+  * const debouncedSearch = debounce(handleSearch, 300);
+  */
+export function debounce<T extends (...args: any[]) => void>(
+  fn: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>): void => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
