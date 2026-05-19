fcf23ca13578f41adcfddcef865d425ea4e1f071
Angarius <angelo.prebianca@gmail.com>
2026-05-19 00:44:37 -0300

    Adicionado BASE_URL nas rotas para deploy no GH pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -18,4 +18,6 @@ export const routes: RouteObject[] = [
   },
 ];
 
-export const router = createBrowserRouter(routes);
+export const router = createBrowserRouter(routes, {
+  basename: import.meta.env.BASE_URL,
+});
