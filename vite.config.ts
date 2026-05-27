50c60b9eee89c7046b3d636e8e2f3195d1875270
Vitor Adriel Deganelli Espinoza Moya <T3756460@timbrasil.com.br>
2026-05-27 13:37:08 -0300

    fix: fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
