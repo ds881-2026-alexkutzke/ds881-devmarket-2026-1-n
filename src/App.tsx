d1aefcdea25b0b687de71686db872b1a0fb654de
Arthur Deretti <arthurderetti05@gmail.com>
2026-05-30 10:20:02 -0300

    Adição de cart-provider e implementação de estado global para o carrinho usando Zustand, com persistência no localStorage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
+import { CartProvider } from './components/CartProvider';
 
 function App() {
 
   return (
-    <div className="app-container">
-      <Outlet />
-      <Footer />
-    </div>
+    <CartProvider>
+      <div className="app-container">
+       <Outlet />
+        <Footer />
+      </div>
+    </CartProvider>
   );
 }
 
